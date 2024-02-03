ed081e24b4b5807fd3a4ffae55d950792bc0f45c
WestGamerPlayZ <120821360+WestGamerPlayZ@users.noreply.github.com>
2024-02-03 10:22:46 +0000

    Made the calculator functional, added a dark mode.

--- script.js
+++ script.js
@@ -8,11 +8,68 @@ function getRadioValue(id) {
     }
 }
 
-function generateOutput() {
-    var n1 = document.getElementById("n1").value;
-    var n2 = document.getElementById("n2").value;
-    var operator = getRadioValue("operation");
-    var output = document.getElementById("display");
+function getResult(n1, n2, operator) {
+    switch(operator) {
+        case "+":
+            return n1 + n2;
+        case "-":
+            return n1 - n2;
+        case "*":
+            return n1 * n2;
+        case "/":
+            return n1 / n2;
+        case "//":
+            return Math.floor(n1 / n2);
+        case "%":
+            return n1 % n2;
+        case "^":
+            return Math.pow(n1, n2);
+        case "==":
+            return n1 == n2;
+        case "===":
+            return n1 === n2;
+        case "!=":
+            return n1 != n2;
+        case "!==":
+            return n1 !== n2;
+        case "<":
+            return n1 < n2;
+        case "<=":
+            return n1 <= n2;
+        case ">":
+            return n1 > n2;
+        case ">=":
+            return n1 >= n2;
+    }
+}
 
-    output.innerHTML = n1 + " " + operator + " " + n2;
+function generateOutput() {
+    var n1 = Number(document.getElementById("n1").value);
+    var n2 = Number(document.getElementById("n2").value);
+
+    var operator = getRadioValue("operation");
+
+    var outputE = document.getElementById("e");
+    var outputR = document.getElementById("r");
+
+    var e = n1 + " " + operator + " " + n2;
+    var r = getResult(n1, n2, operator);
+
+    outputE.innerHTML = e;
+    outputR.innerHTML = r;
+}
+
+function applySettings() {
+    var theme = getRadioValue("theme");
+
+    switch(theme) {
+        case "white":
+            document.body.style.backgroundColor = "#FFFFFF";
+            document.body.style.color = "#000000";
+            break;
+        case "black":
+            document.body.style.backgroundColor = "#000000";
+            document.body.style.color = "#FFFFFF";
+            break;
+    }
 }
